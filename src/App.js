5b98f4a4598f73cc1e69b5963eaca74582a3deac
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-01-21 01:18:24 +0530

    added vite

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pizza = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h3", {}, props.name),
+        React.createElement("h1", {}, props.name),
         React.createElement("p", {}, props.description),
     ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {}, [
-            React.createElement("h1", {}, "Pixel Perfect Pizzas"),
-            React.createElement(Pizza, {
-                name: "Chicken",
-                description: "Large"
-            }),
-            React.createElement(Pizza, {
-                name: "Fish",
-                description: "Small"
-            }),
-            React.createElement(Pizza, {
-                name: "Cheese",
-                description: "Large X 2"
-            }),
-            React.createElement(Pizza, {
-                name: "Pepperoni",
-                description: "Medium"
-            }),
-        ]);
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Padre Gino's"),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: "Mozzarella Cheese, Pepperoni",
+        }),
+        React.createElement(Pizza, {
+            name: "The Hawaiian Pizza",
+            description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+        }),
+        React.createElement(Pizza, {
+            name: "The Big Meat Pizza",
+            description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+        }),
+    ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
