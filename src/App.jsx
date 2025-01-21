4704c308f694255f40fc15eb294216abd08c43dc
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-01-21 23:34:41 +0530

    add orders

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import Order from "./Order";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Padre Gino's</h1>
+            <Order />
+        </div>
+    )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
