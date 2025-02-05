0ceca92db98c732ea470a31e6d2e5688d4d41755
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-02-05 10:11:54 +0530

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -18,7 +17,4 @@ export default defineConfig({
     allowedHosts: ["URL of ngrok"],
   },
   plugins: [TanStackRouterVite(), react()],
-  test: {
-    environment: "happy-dom",
-  },
 });
